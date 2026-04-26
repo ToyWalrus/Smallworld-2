889038d475d05012309843d07cb8e06cf47d9559
Trevor Howell <tkhowell95@gmail.com>
2026-04-26 15:06:12 -0600

    Region selector impl

--- Unity/Assets/Scripts/Models/Interaction/RegionSelector.cs
+++ Unity/Assets/Scripts/Models/Interaction/RegionSelector.cs
@@ -3,18 +3,74 @@ using System.Threading;
 using System.Threading.Tasks;
 using Smallworld.IO;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityModels;
 
 using SMRegion = Smallworld.Models.Region;
 
 public class RegionSelector : MonoBehaviour, ISelection<SMRegion>
 {
+    private TaskCompletionSource<SMRegion> tsc;
+    private List<SMRegion> validItems;
+
     public Task<SMRegion> SelectAsync(List<SMRegion> items)
     {
-        throw new System.NotImplementedException();
+        return SelectAsync(items, CancellationToken.None);
     }
 
-    public Task<SMRegion> SelectAsync(List<SMRegion> items, CancellationToken cancellationToken)
+    public async Task<SMRegion> SelectAsync(List<SMRegion> items, CancellationToken cancellationToken)
     {
-        throw new System.NotImplementedException();
+        if (tsc != null)
+        {
+            Debug.LogError("Already in selection!");
+            return null;
+        }
+
+        tsc = new(TaskCreationOptions.RunContinuationsAsynchronously);
+        validItems = items;
+
+        try
+        {
+            using (cancellationToken.Register(() => tsc.TrySetCanceled()))
+            {
+                return await tsc.Task;
+            }
+        }
+        finally
+        {
+            validItems = null;
+            tsc = null;
+        }
+    }
+
+    void Update()
+    {
+        if (tsc == null || validItems == null)
+        {
+            return;
+        }
+
+        var mouse = Mouse.current;
+        if (!mouse.leftButton.wasPressedThisFrame)
+        {
+            return;
+        }
+
+        var ray = Camera.main.ScreenPointToRay(mouse.position.value);
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, LayerMask.GetMask("Region")))
+        {
+            var region = hit.collider.GetComponent<Region>();
+            if (region)
+            {
+                if (validItems.Contains(region.GetModel()))
+                {
+                    tsc.TrySetResult(region.GetModel());
+                }
+                else
+                {
+                    Debug.Log("That is not a valid region to select");
+                }
+            }
+        }
     }
 }
