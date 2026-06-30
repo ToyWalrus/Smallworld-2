67b77f349450948b573a32760ecf329a6939b6b6
Trevor Howell <tkhowell95@gmail.com>
2026-06-30 11:31:38 -0600

    Move files in Project to top level

--- watch.ps1
+++
@@ -1,3 +0,0 @@
-# Watches Project/Scripts for changes and rebuilds Smallworld.dll into Unity/Assets/Plugins/Smallworld/
-Set-Location "$PSScriptRoot/Project"
-dotnet watch build -c Release Smallworld.csproj
